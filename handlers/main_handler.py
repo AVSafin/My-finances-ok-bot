10d2cdbeb8792e9ce94495378e8464c701177719
Andrey Safin <39008185-avsafin@users.noreply.replit.com>
2025-01-22 11:20:49 +0000

    Auto-update from Replit

--- handlers/main_handler.py
+++ handlers/main_handler.py
@@ -1,51 +1,30 @@
-from telegram import Update
-from telegram.ext import ContextTypes, ConversationHandler
-from keyboards.keyboards import main_menu_keyboard, credits_menu_keyboard, loan_keyboard, main_keyboard
-from data_storage import load_data
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-from handlers import (
-    LOAN_AMOUNT,
-    SHOW_DATA,
-    DELETE_LOAN,
-    CHOOSING_ACTION
-)
-async def choose_action(update: Update, context: ContextTypes.DEFAULT_TYPE):
-   query = update.callback_query
-   if query:
-    await query.answer()
-    if query.data == "calculate_loan":
-        await context.bot.send_message(
-            chat_id=update.effective_chat.id,
-            text="Введите сумму кредита:",
-            reply_markup=loan_keyboard
-        )
-        return LOAN_AMOUNT
-    elif query.data == "show_loans":
-         data = load_data()
-         if "loans" in data and data["loans"]:
-            keyboard = []
-            for index, loan in enumerate(data["loans"]):
-               keyboard.append([InlineKeyboardButton(text = f"Кредит {index + 1}", callback_data=f"show_loan_{index}")])
-            keyboard.append([InlineKeyboardButton("Главное меню", callback_data="start")])
-            await query.edit_message_text(text="Выберите кредит для просмотра:", reply_markup=InlineKeyboardMarkup(keyboard))
-         else:
-             await query.edit_message_text(text="Нет сохраненных кредитов.", reply_markup=main_keyboard)
-         return SHOW_DATA
-    elif query.data == "delete_loans":
-         data = load_data()
-         if "loans" in data and data["loans"]:
-            keyboard = []
-            for index, loan in enumerate(data["loans"]):
-                keyboard.append([InlineKeyboardButton(text = f"Кредит {index + 1}", callback_data=f"delete_loan_{index}")])
-            keyboard.append([InlineKeyboardButton("Главное меню", callback_data="start")])
-            await query.edit_message_text(text="Выберите кредит для удаления:", reply_markup=InlineKeyboardMarkup(keyboard))
-         else:
-            await query.edit_message_text(text="Нет сохраненных кредитов.", reply_markup=main_keyboard)
-         return DELETE_LOAN
-   else:
-        await context.bot.send_message(
-          chat_id=update.effective_chat.id,
-          text="Выберите действие:",
-          reply_markup = main_keyboard
-       )
-        return CHOOSING_ACTION
+from telegram import Update, ReplyKeyboardMarkup
+from telegram.ext import ContextTypes
+import logging
+from keyboards.keyboards import credits_menu_keyboard, main_keyboard, show_keyboard, loan_keyboard
+from handlers import calculate_loan_handler, show_data_handler, delete_loan_handler
+
+
+# Choose action depending on user message
+async def choose_action(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    user_text = update.message.text
+    logging.info(f"Получено сообщение от пользователя {update.message.from_user.username}: {user_text}")
+
+
+    if user_text == "Кредиты":
+        reply_markup = credits_menu_keyboard
+        await update.message.reply_text("Выбери действие с кредитом:", reply_markup=reply_markup)
+    elif user_text == "Сбережения":
+        await update.message.reply_text("Раздел в разработке")
+    elif user_text == "Прогнозирование":
+        await update.message.reply_text("Раздел в разработке")
+    elif user_text == "Добавить кредит":
+        await calculate_loan_handler.loan_amount(update, context)
+    elif user_text == "Просмотреть кредиты":
+       await show_data_handler.show_data(update, context)
+    elif user_text == "Удалить кредит":
+       await delete_loan_handler.delete_loan(update, context)
+    elif user_text == "Назад":
+        await update.message.reply_text("Вы в главном меню", reply_markup=main_keyboard)
+    else:
+        await update.message.reply_text("Неизвестная команда")
